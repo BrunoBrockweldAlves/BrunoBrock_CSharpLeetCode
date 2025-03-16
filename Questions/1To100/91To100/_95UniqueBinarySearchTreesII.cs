38875f35592ba9162e294658e93b73e77876c8cd
bruno7283 <bruno7283@hotmail.com>
2025-03-16 16:03:33 -0300

    Fix class name + development

---
+++ Questions/1To100/91To100/_95UniqueBinarySearchTreesII.cs
@@ -0,0 +1,42 @@
+﻿using BaseClasses.BinaryTrees;
+
+namespace Questions._1To100._91To100
+{
+    public class _95UniqueBinarySearchTreesII
+    {
+        public IList<TreeNode> GenerateTrees(int n)
+        {
+            var result = new List<TreeNode>();
+            for (int i = n; i > 0; i--)
+            {
+
+            }
+
+            // TODO remove
+            return default;
+
+            void BuildAllCombinations(int value)
+            {
+                // Create only lefts
+                for (int i = value; i > 0; i--)
+                {
+                    var left = new TreeNode();
+                    new TreeNode(value);
+                }
+
+                // Create only rights
+                for (int i = value; i > 0; i--)
+                {
+                    new TreeNode(i);
+                }
+
+                // Create LeftRights
+                for (int i = value; i > 0; i--)
+                {
+                    new TreeNode(i);
+                }
+            }
+
+        }
+    }
+}
